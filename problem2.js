9e0e5e3487ad596e5aa3965063eb5b1c7d7f4fce
Alexander Marston <amarstonbanyan@gmail.com>
2023-11-29 18:29:45 -0600

    added problem 3

--- problem2.js
+++ problem2.js
@@ -1,1 +1,16 @@
-console.log('Finished Also')
+let arr = [
+    {name: 'alex', age: 35},
+    {name: 'tom', age: 35},
+    {name: 'jonny', age: 32},
+    {name: 'joe', age: 27},
+    {name: 'chris', age: 18},
+    {name: 'alex', age: 65},
+    {name: 'gabby', age: 22}
+]
+
+let filteredArray = arr.map((item) => {
+    item.id = "";
+    return item;
+})
+
+console.log(filteredArray)
